84ac981412eb216d3eeef1002fb101f4967e7cc1
Lymia Kanokawa <lymia@lymia.moe>
2024-04-07 15:55:13 -0700

    Add splitter tuning parameters to the CLI.

--- mkwebfont/src/main.rs
+++ mkwebfont/src/main.rs
@@ -2,7 +2,7 @@ use clap::Parser;
 use mkwebfont::SplitWebfontCtx;
 use roaring::RoaringBitmap;
 use std::{fmt::Write, io, path::PathBuf};
-use tracing::{info, warn};
+use tracing::{error, info, warn};
 
 /// Generates webfonts for a given font.
 #[derive(Parser, Debug)]
@@ -11,9 +11,13 @@ struct Args {
     /// The font files to generate webfonts from.
     fonts: Vec<PathBuf>,
 
-    /// The location to store generated woff2 files in.
+    /// The location to store generated .woff2 files in.
     #[arg(short, long)]
-    store: PathBuf,
+    store: Option<PathBuf>,
+
+    /// The URI at which the .woof2 store can be accessed at.
+    #[arg(long)]
+    store_uri: Option<String>,
 
     /// Where to write the generated .css file.
     #[arg(short, long)]
@@ -23,15 +27,21 @@ struct Args {
     #[arg(short, long)]
     verbose: bool,
 
-    /// The URI to prepend to the generated .css file.
-    #[arg(long)]
-    store_uri: Option<String>,
-
     /// Always include a list of codepoints in the first partition split off from the font
-    /// (usually latin). This can be used to allow unusual characters used throughout a website
-    /// to be immediately available, rather than requiring loading another woff font.
+    /// (usually latin).
+    ///
+    /// This can be used to allow unusual characters used throughout a website to be immediately
+    /// available, rather than requiring loading another .woff2 font.
     #[arg(long)]
     preload: Vec<String>,
+
+    /// Uses the splitter tuning file at the given path.
+    #[arg(long)]
+    splitter_tuning: Option<PathBuf>,
+
+    /// Writes the default splitter tuning file to the given path.
+    #[arg(long)]
+    write_default_splitter_tuning: Option<PathBuf>,
 }
 
 fn main() {
@@ -41,10 +51,23 @@ fn main() {
         .with_writer(io::stderr)
         .init();
 
+    // write splitter configuration
+    if let Some(path) = args.write_default_splitter_tuning {
+        info!("Writting default splitter configuration to {}", path.display());
+        std::fs::write(path, include_str!("splitter_default_tuning.toml")).unwrap();
+        return;
+    }
+
+    // check arguments
+    if args.store.is_none() {
+        error!("`--store <STORE>` parameter must be provided.");
+        std::process::exit(1)
+    }
     if args.fonts.is_empty() {
         warn!("No fonts were specified! An empty .css file will be generated.");
     }
 
+    // do actual webfont generaetion
     let mut css = String::new();
     let store_uri = if let Some(store_uri) = args.store_uri {
         store_uri
@@ -60,10 +83,15 @@ fn main() {
     }
 
     let mut split_ctx = SplitWebfontCtx::default();
+    if let Some(tuning) = args.splitter_tuning {
+        split_ctx.splitter_tuning = Some(std::fs::read_to_string(tuning).unwrap());
+    }
     split_ctx.preload_codepoints = preload_codepoints;
     for font in &args.fonts {
         info!("Processing webfont: {}", font.display());
-        for data in mkwebfont::split_webfont(&split_ctx, font, &args.store).unwrap() {
+        for data in
+            mkwebfont::split_webfont(&split_ctx, font, args.store.as_ref().unwrap()).unwrap()
+        {
             writeln!(css, "{}", data.render_css(&store_uri)).unwrap();
         }
     }
