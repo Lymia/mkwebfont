67ba1ed52cb21cc1e4ba53ea5dcd6d92605031b6
Lymia Kanokawa <lymia@lymia.moe>
2024-04-07 04:18:08 -0700

    Improve command line interface.

--- mkwebfont/src/lib.rs
+++ mkwebfont/src/lib.rs
@@ -8,11 +8,14 @@ mod woff2;
 pub use font_ops::{FontStyle, FontWeight};
 pub use splitter::{FontStylesheetEntry, FontStylesheetInfo};
 
-pub fn split_webfont(path: std::path::PathBuf) -> anyhow::Result<FontStylesheetInfo> {
+pub fn split_webfont(
+    font_path: &std::path::Path,
+    store_path: &std::path::Path,
+) -> anyhow::Result<FontStylesheetInfo> {
     splitter::split_webfont(
         None,
         ranges::WebfontDataCtx::load(),
-        &std::fs::read(path)?,
-        &std::path::PathBuf::from("run"),
+        &std::fs::read(font_path)?,
+        store_path,
     )
 }
--- mkwebfont/src/main.rs
+++ mkwebfont/src/main.rs
@@ -1,23 +1,53 @@
 use clap::Parser;
-use std::path::PathBuf;
+use std::{fmt::Write, io, path::PathBuf};
+use tracing::info;
 
 /// Generates webfonts for a given font.
 #[derive(Parser, Debug)]
 #[command(version, about, long_about = None)]
 struct Args {
-    /// The font file or .toml spec to generate webfonts from
-    font: PathBuf,
+    /// The font files to generate webfonts from.
+    fonts: Vec<PathBuf>,
+
+    /// The location to store generated woff2 files in.
+    #[arg(short, long)]
+    store: PathBuf,
+
+    /// Where to write the generated .css file.
+    #[arg(short, long)]
+    output: Option<PathBuf>,
 
     /// Whether to enable verbose output
     #[arg(short, long)]
     verbose: bool,
+
+    /// The URI to prepend to the generated .css file.
+    #[arg(long)]
+    store_uri: Option<String>,
 }
 
 fn main() {
     let args = Args::parse();
     tracing_subscriber::fmt()
         .with_env_filter(if args.verbose { "mkwebfont=debug,info" } else { "info" })
+        .with_writer(io::stderr)
         .init();
-    let data = mkwebfont::split_webfont(args.font).unwrap();
-    println!("{}", data.render_css(""));
+
+    let mut css = String::new();
+    let store_uri = if let Some(store_uri) = args.store_uri {
+        store_uri
+    } else {
+        String::new()
+    };
+    for font in &args.fonts {
+        info!("Processing webfont: {}", font.display());
+        let data = mkwebfont::split_webfont(font, &args.store).unwrap();
+        writeln!(css, "{}", data.render_css(&store_uri)).unwrap();
+    }
+
+    if let Some(target) = args.output {
+        std::fs::write(target, css).unwrap();
+    } else {
+        println!("{}", css);
+    }
 }
