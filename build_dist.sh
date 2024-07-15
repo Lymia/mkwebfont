7d83fbdacb1ff0c1360850baefe614e5c4f4556e
Rimi Kanokawa <rimi@rimin.moe>
2024-07-15 15:31:55 -0700

    Use alpine build instead of nix.

--- build_dist.sh
+++ build_dist.sh
@@ -3,16 +3,17 @@
 VERSION="0.2.0-alpha5"
 SHORT_VERSION="$(echo "$VERSION" | sed "s/-.*//g")"
 
-rm -v mkwebfont*.AppImage
 rm -rfv dist
-
 mkdir dist
 
-nix develop -c cargo zigbuild -p mkwebfont --target x86_64-pc-windows-gnu --release || exit 1
-nix bundle --bundler github:ralismark/nix-appimage?rev=17dd6001ec228ea0b8505d6904fc5796d3de5012 .?submodules=1#mkwebfont --impure || exit 1
+cargo zigbuild -p mkwebfont --target x86_64-unknown-linux-musl --release || exit 1
+cargo zigbuild -p mkwebfont --target aarch64-unknown-linux-musl --release || exit 1
+cargo zigbuild -p mkwebfont --target x86_64-apple-darwin --release || exit 1
+cargo zigbuild -p mkwebfont --target aarch64-apple-darwin --release || exit 1
+cargo zigbuild -p mkwebfont --target x86_64-pc-windows-gnu --release || exit 1
 
-cp -v target/x86_64-pc-windows-gnu/release/mkwebfont.exe dist/"mkwebfont-$VERSION-x86_64.exe" || exit 1
-cp -v "mkwebfont-$SHORT_VERSION-x86_64.AppImage" dist/"mkwebfont-$VERSION-x86_64.AppImage" || exit 1
-
-chmod 755 dist/*
-rm -v mkwebfont*.AppImage
+cp -v target/x86_64-unknown-linux-musl/release/mkwebfont dist/"mkwebfont-$VERSION-x86_64-linux" || exit 1
+cp -v target/aarch64-unknown-linux-musl/release/mkwebfont dist/"mkwebfont-$VERSION-aarch64-linux" || exit 1
+cp -v target/x86_64-apple-darwin/release/mkwebfont dist/"mkwebfont-$VERSION-x86_64-macos" || exit 1
+cp -v target/aarch64-apple-darwin/release/mkwebfont dist/"mkwebfont-$VERSION-aarch64-macos" || exit 1
+cp -v target/x86_64-pc-windows-gnu/release/mkwebfont.exe dist/"mkwebfont-$VERSION-x86_64-win32.exe" || exit 1
