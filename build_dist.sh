0871a82472c4931149cd5c1821c82fe5cabe1172
Rimi Kanokawa <rimi@rimin.moe>
2024-07-15 15:56:28 -0700

    Use a generic BIN_NAME environment field.

--- build_dist.sh
+++ build_dist.sh
@@ -1,19 +1,20 @@
 #!/bin/sh
 
+BIN_NAME="mkwebfont"
 VERSION="0.2.0-alpha5"
 SHORT_VERSION="$(echo "$VERSION" | sed "s/-.*//g")"
 
 rm -rfv dist
 mkdir dist
 
-cargo zigbuild -p mkwebfont --target x86_64-unknown-linux-musl --release || exit 1
-cargo zigbuild -p mkwebfont --target aarch64-unknown-linux-musl --release || exit 1
-cargo zigbuild -p mkwebfont --target x86_64-apple-darwin --release || exit 1
-cargo zigbuild -p mkwebfont --target aarch64-apple-darwin --release || exit 1
-cargo zigbuild -p mkwebfont --target x86_64-pc-windows-gnu --release || exit 1
+cargo zigbuild -p $BIN_NAME --target x86_64-unknown-linux-musl --release || exit 1
+cargo zigbuild -p $BIN_NAME --target aarch64-unknown-linux-musl --release || exit 1
+cargo zigbuild -p $BIN_NAME --target x86_64-apple-darwin --release || exit 1
+cargo zigbuild -p $BIN_NAME --target aarch64-apple-darwin --release || exit 1
+cargo zigbuild -p $BIN_NAME --target x86_64-pc-windows-gnu --release || exit 1
 
-cp -v target/x86_64-unknown-linux-musl/release/mkwebfont dist/"mkwebfont-$VERSION-x86_64-linux" || exit 1
-cp -v target/aarch64-unknown-linux-musl/release/mkwebfont dist/"mkwebfont-$VERSION-aarch64-linux" || exit 1
-cp -v target/x86_64-apple-darwin/release/mkwebfont dist/"mkwebfont-$VERSION-x86_64-macos" || exit 1
-cp -v target/aarch64-apple-darwin/release/mkwebfont dist/"mkwebfont-$VERSION-aarch64-macos" || exit 1
-cp -v target/x86_64-pc-windows-gnu/release/mkwebfont.exe dist/"mkwebfont-$VERSION-x86_64-win32.exe" || exit 1
+cp -v target/x86_64-unknown-linux-musl/release/$BIN_NAME dist/"$BIN_NAME-$VERSION-x86_64-linux" || exit 1
+cp -v target/aarch64-unknown-linux-musl/release/$BIN_NAME dist/"$BIN_NAME-$VERSION-aarch64-linux" || exit 1
+cp -v target/x86_64-apple-darwin/release/$BIN_NAME dist/"$BIN_NAME-$VERSION-x86_64-macos" || exit 1
+cp -v target/aarch64-apple-darwin/release/$BIN_NAME dist/"$BIN_NAME-$VERSION-aarch64-macos" || exit 1
+cp -v target/x86_64-pc-windows-gnu/release/$BIN_NAME.exe dist/"$BIN_NAME-$VERSION-x86_64-win32.exe" || exit 1
